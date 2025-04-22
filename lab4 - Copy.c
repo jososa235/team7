2daa2cdaf7cf3b95107b41f90d516c62e3cd66da
Jordan Sosa <jjsosa@usc.edu>
2025-04-21 18:21:59 -0700

    added main  user_input function to possibly save around 300 lines of code

--- lab4 - Copy.c
+++ lab4 - Copy.c
@@ -18,9 +18,6 @@
 * 11/18/13 A. Weber    Renamed for ATmega328P
 *************************************************************/
 #include <string.h>
-#include <stdio.h>
-#include <stdbool.h>
-
 #define FOSC 7372800
 
 #define BDIV (((FOSC / 100000) - 16) / 2 + 1)
@@ -144,81 +141,6 @@ void lcd_clear(void) {
     _delay_ms(2);        // Wait for LCD to process (needs about 1.5ms)
 }
 
-
-//potential code cleanup code
-void read_input(char input[], bool *hash_flag, bool *star_flag){
-    bool flag_passcode_correct = false;
-    int user_index = 0;
-    //char user_input[10];
-    while(!flag_passcode_correct){ // wait for hashtag
-        if(flag_tca_int){
-            flag_tca_int = false;
-            //lcd_set_cursor(0x14);
-            //lcd_print("In flag loop");
-            //lcd_set_cursor(0x5C); //setting cursor back for passcode entry display
-        
-            // reading int_stat to ACK receipt of int
-            uint8_t int_stat_reg = 0x02;
-            uint8_t int_status = 0;
-            i2c_io(0x69, &int_stat_reg, 1, &int_status, 1);
-        
-            // checking if key event (0x01) is the cause of the interrupt
-            if (int_status & 0x01) {
-                // reading fifo
-                uint8_t fifo_cnt_reg = 0x03;
-                uint8_t fifo_cnt = 0;
-                i2c_io(0x69, &fifo_cnt_reg, 1, &fifo_cnt, 1);
-                fifo_cnt &= 0x0F;
-                int i;
-                for (i = 0; i < fifo_cnt; i++) {
-                    uint8_t fifo_reg = 0x04;
-                    uint8_t key_event = 0;
-                    i2c_io(0x69, &fifo_reg, 1, &key_event, 1);
-        
-                    if (key_event & 0x80) {
-                        key_event &= 0x7F;
-                        int row = (key_event - 1) / 10;
-                        int col = (key_event - 1) % 10;
-                        char ch = keypad[row][col];
-                        char buf[4];
-                        sprintf(buf, "%c", ch);
-                        lcd_print(buf); //printing in real time
-
-                        if(ch == '#'){
-                            //an idea can just return '#' having been pressed aswell same for '*' and return user_input for that 
-
-                            //all states just use # as a flag so no more logic needed
-                            *hash_flag = true; //changes flag pointer outside this functoin logic
-                            break;
-
-                        }else if(ch == '*'){ //action * button
-                            input[user_index] = '\0';
-                            //lcd output is code matches
-                            
-                            *star_flag = true;
-
-                            //lcd_set_cursor(0x54);
-                            //lcd_print("IN:");
-                            //char temp[10];
-                            //sprintf(temp, "%10s", user_input); 
-                            //lcd_print();
-                            //lcd_print(user_input);       // Reset input
-                            //flag_passcode_correct = false;
-                            
-                        } else {
-                            if (user_index < sizeof(input) - 1) {
-                                input[user_index++] = ch;  // Append char to string
-                            }
-                        }
-                    }
-                }
-            }
-            //writing to INT_STAT to clear interrupt bits
-            uint8_t clear_int[] = {0x02, 0x1F};
-            i2c_io(0x68, clear_int, 2, NULL, 0);
-        }    
-    }
-}
 
 //updated tca module
 void tca8418_init() {
@@ -680,13 +602,7 @@ int main(void)
                                     char buf[4];
                                     sprintf(buf, "%c", ch);
                                     lcd_print(buf); //printing in real time
-                                    if(ch == '#'){
-                                        lcd_set_cursor(0x54);
-                                        lcd_print("RESET CANCELLED");
-                                        flag_passcode_correct = true;
-                                        _delay_ms(1000);
-                                        current_state = state_unlocked;
-                                    }else if(ch == '*'){
+                                    if(ch == '*'){
                                         user_input[user_index] = '\0';
                                         //copying code over (resetting code)
                                         memcpy(global_code, user_input, sizeof(user_input));  // copies all elements
