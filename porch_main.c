8046d0719c6c1a6e3f424e04f10bdd963b4b3273
Jordan Sosa <jjsosa@usc.edu>
2025-04-24 10:46:31 -0700

    small comment changes, name changes

--- porch_main.c
+++ porch_main.c
@@ -1,21 +1,11 @@
 /*************************************************************
-*       at328-0.c - Demonstrate simple I/O functions of ATmega328
-*
-*       Program loops turning PC0 on and off as fast as possible.
-*
-* The program should generate code in the loop consisting of
-*   LOOP:   SBI  PORTC,0        (2 cycles)
-*           CBI  PORTC,0        (2 cycles)
-*           RJMP LOOP           (2 cycles)
-*
-* PC0 will be low for 4 / XTAL freq
-* PC0 will be high for 2 / XTAL freq
-* A 9.8304MHz clock gives a loop period of about 600 nanoseconds.
-*
-* Revision History
-* Date     Author      Description
-* 09/14/12 A. Weber    Initial Release
-* 11/18/13 A. Weber    Renamed for ATmega328P
+* Porch Protector Main File
+* USC EE459 Capstone Project
+* Team 7
+* Contributors: Jordan Sosa, Matthew Guo, Kevin Luo
+* 
+* Built using avr-gcc for ATMega328P Flashing
+* 
 *************************************************************/
 #include <string.h>
 #include <stdio.h>
